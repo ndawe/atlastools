cd04299cbfb73992537d5356a17ac6b5cab59227
Noel Dawe <Noel.Dawe@cern.ch>
2011-08-03 05:26:51 -0700

    updates

---
+++ atlastools/batch.py
@@ -0,0 +1,8 @@
+from rootpy.batch import Student
+
+class ATLASStudent(Student):
+
+    def __init__(self, *args, **kwargs):
+
+        super(ATLASStudent, self).__init__(*args, **kwargs)
+        self.grl = kwargs.get('grl', None)
