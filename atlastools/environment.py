604fb8277ecf75415be87850ee92da8c9d25ddd5
Noel Dawe <Noel.Dawe@cern.ch>
2011-08-16 01:54:19 -0700

    updates

--- atlastools/environment.py
+++ atlastools/environment.py
@@ -4,10 +4,13 @@ Pickle your current shell environment for later use.
 import cPickle
 import sys
 import os
-import commands
+import subprocess
 
 DISPLAY = os.getenv("DISPLAY",'')
-PATH = os.path.join(os.getenv("ATLASTOOLS_CONFIG_ROOT","HOME"), 'env')
+ATLASTOOLS_ROOT = os.getenv("ATLASTOOLS_CONFIG_ROOT",None)
+if not ATLASTOOLS_ROOT:
+    ATLASTOOLS_ROOT = os.getenv("HOME")
+PATH = os.path.join(ATLASTOOLS_ROOT, 'env')
 
 def define(name):
     """
@@ -15,7 +18,8 @@ def define(name):
     """
     if not os.path.exists(PATH):
         os.makedirs(PATH)
-    env = {"System":os.environ, "Python":sys.path}
+    aliases = subprocess.check_output(["alias", "-p"])
+    env = {"System":os.environ, "Python":sys.path, "aliases": aliases}
     env_file = open((os.path.join(PATH, "%s.env"))% name,'wb')
     cPickle.dump(env, env_file)
     env_file.close()
@@ -31,11 +35,11 @@ def load(name):
     env_file = open(env_filename, 'rb')
     new_env = cPickle.load(env_file)
     env_file.close()
-    current_python = commands.getoutput("which python")
+    current_python = subprocess.check_output(["which", "python"])
     os.environ.clear()
     os.environ.update(new_env["System"])
     os.environ["DISPLAY"] = DISPLAY
-    new_python = commands.getoutput("which python")
+    new_python = subprocess.check_output(["which", "python"])
     sys.path = new_env["Python"]
     print "Environment %s has been loaded."% name
     if new_python != current_python:
